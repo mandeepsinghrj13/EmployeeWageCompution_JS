0d20fed1444c31241d387a3e727fc0da1a699503
mandeep <singh.mr.1996@gmail.com>
2021-08-25 16:27:10 +0530

    Added: refactor the code to write a function to get work hours

--- EmployeeWageCalculation.js
+++ EmployeeWageCalculation.js
@@ -1,21 +1,22 @@
-//Calculating Employee Wage UC 2
+//Refactored code to write function for daily working hours
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHours = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-  case IS_PART_TIME:
-    empHours = PART_TIME_HOURS;
-    break;
-  case IS_FULL_TIME:
-    empHours = FULL_TIME_HOURS;
-    break;
-  default:
-    empHours = 0;
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
+    case IS_PART_TIME:
+      return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+      return FULL_TIME_HOURS;
+    default:
+      return 0;
+  }
 }
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHours = getWorkingHours(empCheck);
 let empWage = empHours * WAGE_PER_HOUR;
 console.log("Employee Wage " + empWage);
